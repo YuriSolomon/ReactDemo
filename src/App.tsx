240917b58c2b503d6f5c3e07372d3b87d082518e
YuriSolomon <yuribarak.solomon@infosys.com>
2025-03-19 10:45:09 +0100

    get course materials into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const reactDescriptions: String[] = ['Fundamental', 'Crucial', 'Core'];
 
-function Header() {
-  return (
-    <header>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </header>
-  )
-}
-
-const optionsList: String[] = ['count is', 'number of clicks', 'hits'];
-
-function getRamdonInt(max: any) {
+function genRandomInt(max: any) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function App() {
-  const [count, setCount] = useState(0)
-  const optionString: String = optionsList[getRamdonInt(optionsList.length - 1)]
+function Header() {
+  const description: String = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
 
   return (
-    <>
-      <Header/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {optionString} {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
-    </>
-  )
+    </header>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
