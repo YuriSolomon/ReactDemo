8aec7c3931fcd305733a4d3af546388d6ae5cdb8
YuriSolomon <juristein21@gmail.com>
2025-03-19 14:45:24 +0100

    separate components to different files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,6 @@
-import reactImage from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.ts';
-
-const reactDescriptions: String[] = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max: any) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description: String = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}: any) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header.tsx';
+import CoreConcept from './components/CoreConcept.tsx';
 
 function App() {
   return (
